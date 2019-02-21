1f09205480e83440cdcbc86795fba610c814a170
Joel Richardson <joel.richardson@jax.org>
2019-02-21 12:21:55 -0500

    Add namespace so keys can be grouped by genome.

--- src/lib/CachingFetcher.js
+++ src/lib/CachingFetcher.js
@@ -4,16 +4,18 @@ import KeyStore from '@/lib/KeyStore'
 class CachingFetcher {
   // Args:
   //  cacheName (string) Name to use for the IndexedDB cache
-  constructor (cacheName) {
+  //  namespace (string) optional. If provided, prefixes all cache keys with this value.
+  constructor (cacheName, namespace) {
     this.kstore = new KeyStore(cacheName)
+    this.namespace = namespace || ''
   }
   // Returns a promise for the content at the specified url.
   // Returns results from the cache if found, else fetches over the net, then caches and returns it.
   // Args:
   //   url (string) the URL to fetch
   //   type (string) one of: text, json, gff3
-  fetch (url, type) {
-    const key = `${type}::${url}`
+  fetch (url, type, namespace) {
+    const key = `${this.namespace}::${type}::${url}`
     return this.kstore.get(key).then(cachedval => {
       if (cachedval === undefined) {
         return u.fetch(url, type).then(val => {
@@ -25,9 +27,12 @@ class CachingFetcher {
       }
     })
   }
-  // Clears the url cache
-  clear () {
-    return this.kstore.clear()
+  // Removes all entries under my namespace. Returns a promise that resolves when all keys removed.
+  clearNamespace () {
+    return this.kstore.keys().then(keys => {
+      const mykeys = keys.filter(k => k.startsWith(this.namespace+'::'))
+      return Promise.all(mykeys.map(k => this.kstore.del(k)))
+    })
   }
 }
 
