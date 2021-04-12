mgijax/mgv

commit fe82b752df84f4bfe627ce49f92c5049a082d0b0

Set default coordinates in setContext if none are provided.

src/components/MGV.vue
@@ -563,14 +563,22 @@ export default MComponent({
       }
       //
       let p
-      if (this.rGenome && cxt.chr) {
-        const c = this.rGenome.chromosomes.filter(chr => chr.name === cxt.chr)[0]
-        const coords = {
-          chr: c,
-          start: cxt.start,
-          end: cxt.end
-        }
-        p = this.regionManager.initMappedRegions(this.rGenome, coords, genomes)
+      if (this.rGenome) {
+        p = this.dataManager.ensureFeatures(this.rGenome).then(() => {
+            if (!cxt.chr) {
+                // if no coordinates specified, insert default. First megabase of the first chr
+                cxt.chr = this.rGenome.chromosomes[0].name
+                cxt.start = this.rGenome.chromosomes[0].minStart
+                cxt.end = cxt.start + 1000000
+            }
+            const c = this.rGenome.chromosomes.filter(chr => chr.name === cxt.chr)[0]
+            const coords = {
+              chr: c,
+              start: cxt.start,
+              end: cxt.end
+            }
+            return this.regionManager.initMappedRegions(this.rGenome, coords, genomes)
+        })
       } else if (cxt.landmark) {
         const lcoords = {
            landmark: cxt.landmark,

src/lib/DataManager.js
@@ -577,6 +577,8 @@ class FeatureRegistrar {
     // each chromosome of each genome has its own registrar
     this.genome = g
     this.chr = c
+    c.minStart = c.length
+    c.maxEnd = 0
     // map feature.ID => feature
     this.id2feat = id2f
     // map feature.cID => [ features ]
@@ -593,6 +595,9 @@ class FeatureRegistrar {
     f.sotype = f.type
     f.genome = this.genome
     f.chr = this.chr
+    //
+    this.chr.minStart = Math.min(this.chr.minStart, f.start)
+    this.chr.maxEnd = Math.max(this.chr.maxEnd, f.end)
     //
     f.length = f.end - f.start + 1
     if (f.length > config.DataManager.featureSizeLimit) {

commit 26b3db9324ee89a25de1c9ec76b830a055727806

Fix bug introduced by fe82b752df84f4bfe627ce49f92c5049a082d0b0.

src/components/MGV.vue
@@ -563,7 +563,7 @@ export default MComponent({
       }
       //
       let p
-      if (this.rGenome) {
+      if (this.rGenome && !cxt.landmark) {
         p = this.dataManager.ensureFeatures(this.rGenome).then(() => {
             if (!cxt.chr) {
                 // if no coordinates specified, insert default. First megabase of the first chr