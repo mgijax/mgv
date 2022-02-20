d80bfb1757dbc227e2bb41d4f591bec9677eb599
Joel Richardson <joel.richardson@jax.org>
2022-02-20 05:56:21 -0500

    Fix row open/close to remember state across paginations.

--- src/components/FeatureDetails.vue
+++ src/components/FeatureDetails.vue
@@ -26,7 +26,8 @@
             @m-paginator-changed="pageChange"/>
     </div>
     <table class="feature-table">
-      <tr class="sticky-header" @click="toggleCollapsed">
+      <tr class="sticky-header"
+          @click="toggleCollapsed">
         <th v-for="(col, ci) in columns"
             :key="'col.'+ci"
             :style="col.thStyle"
@@ -35,12 +36,12 @@
             {{col.name}}
             <i v-if="col.name==='Attributes'"
                 title="Open all"
-                @click="openAllAttributes"
+                @click.stop="openAllAttributes"
                 class="material-icons"
                 >add_circle</i>
             <i v-if="col.name==='Attributes'"
                 title="Close all"
-                @click="closeAllAttributes"
+                @click.stop="closeAllAttributes"
                 class="material-icons"
                 >remove_circle</i>
         </th>
@@ -48,7 +49,11 @@
       <tr
         v-for="(f,i) in featuresCurrentPage"
         :key="i"
-        class="featureRow"
+        class="feature-row"
+        :class="{ highlighted : featureHighlighted(f) }"
+        @mouseover="overFeature(f, $event)"
+        @mouseout="outFeature(f, $event)"
+        @click.stop="clickFeature(f, $event)"
         >
         <td v-for="(col, ci) in columns"
             :key="'col.td.'+ci"
@@ -58,9 +63,11 @@
             <span v-if="col.name !== 'Attributes'">{{getCellValue(startIndex + i + 1, f, col.value)}}</span>
             <template v-else>
                 <!-- when closed, shows this -->
-                <span class="closed-text">{{ attributesClosedText(f) }}</span>
+                <span v-show="!attrsOpened[startIndex + i]"
+                    class="closed-text"
+                    >{{ attributesClosedText(f) }}</span>
                 <!-- when opened, list attributes one per line -->
-                <table>
+                <table v-show="attrsOpened[startIndex + i]">
                     <tr v-for="(av,j) in attributesAsList(f)"
                       :key= "`f${i}a${j}`"
                       >
@@ -68,8 +75,12 @@
                       <td><span class="crawling-text" @mouseover="startcrawl" @mouseout="stopcrawl" >{{av[1]}}</span></td>
                     </tr>
                 </table>
-                <i @click="clicked" class="material-icons open">add_circle</i>
-                <i @click="clicked" class="material-icons close">remove_circle</i>
+                <i v-show="!attrsOpened[startIndex + i]" class="material-icons open"
+                    @click.stop="clicked(startIndex + i, 'open')"
+                    >add_circle</i>
+                <i v-show="attrsOpened[startIndex + i]" class="material-icons close"
+                    @click.stop="clicked(startIndex + i, 'close')"
+                    >remove_circle</i>
             </template>
         </td>
       </tr>
@@ -91,10 +102,11 @@ export default MComponent({
           showTranscripts: 'selected', // one of: all, selected, none
           showExons: 'selected', // one of: all, selected, none
           collapsed: new Set(), // cellIndex vals for columns to be drawn collapsed
+          attrsOpened: [], // attrsOpened[i] (boolean) expand i-th feature's attributes column
           columns: [{
               name: "#",
               value: "#",
-              tdClass: { collapsed: false }
+              tdClass: { collapsed: false, "unselectable-text" : true }
           }, {
               name: "Genome",
               value: "genome.name",
@@ -187,12 +199,47 @@ export default MComponent({
     }
   },
   methods: {
-    pageChange: function (d) {
+    getEventObjects (f, ev) {
+        const d = {
+            region: null,
+            feature: null,
+            transcript: null,
+            exon: null,
+            event: ev
+        }
+        if (f.transcript) {
+            d.exon = f
+            f = f.transcript
+        }
+        if (f.gene) {
+            d.transcript = f
+            f = f.gene
+        }
+        d.feature = f
+        return d
+    },
+    overFeature (f, ev) {
+        this.$root.$emit('feature-over', this.getEventObjects(f, ev))
+    },
+    outFeature (f, ev) {
+        this.$root.$emit('feature-out', this.getEventObjects(f, ev))
+    },
+    clickFeature(f, ev) {
+        this.$root.$emit('feature-click', this.getEventObjects(f, ev))
+    },
+    featureHighlighted (f) {
+        const a = this.app
+        const cm = a.currentMouseover === f || a.currentMouseoverT === f || a.currentMouseoverE === f
+        const cs = a.csSetH.has(f) || a.csSetT.has(f) || a.csSetE.has(f) 
+        const cmde = f.de && (f.de === a.currentMouseoverE)
+        return cm || cs || cmde
+    },
+    pageChange (d) {
         u.debug('m-paginator-changed', d)
         this.startIndex = d.startIndex
         this.endIndex = d.endIndex
     },
-    getCellValue: function (i, f, name) {
+    getCellValue (i, f, name) {
         if (name === "#") return i
         return name.split('.').reduce((v,n) => {
             if (!v || v[n] === undefined) return '.'
@@ -208,10 +255,6 @@ export default MComponent({
         if (ac !== bc) return ac < bc ? -1 : ac > bc ? 1 : 0
         return a.start - b.start
     },
-    clicked: function (e) {
-        const td = e.target.closest('td.attributes')
-        this.toggleAttributes(td)
-    },
     toggleCollapsed (ev) {
         const th = ev.target.closest('th')
         if (!th) return
@@ -222,29 +265,20 @@ export default MComponent({
     isCollapsed (cellIndex) {
         return this.columns[cellIndex].tdClass.collapsed
     },
-    openAttributes  (td) {
-        td.classList.add('open')
-        td.classList.remove('closed')
+    //
+    clicked (i, which) {
+        this.attrsOpened[i] = (which === 'open')
+        // simple assignment to array element is not observable,
+        // so we add a no-op splice call to force observation
+        this.attrsOpened.splice(0,0)
     },
-    closeAttributes (td) {
-        td.classList.add('closed')
-        td.classList.remove('open')
+    openAllAttributes () {
+        this.attrsOpened = this.features.map(() => true)
     },
-    toggleAttributes: function (td) {
-        if (td.classList.contains('open')) {
-            this.closeAttributes(td)
-        } else {
-            this.openAttributes(td)
-        }
+    closeAllAttributes () {
+        this.attrsOpened = []
     },
-    openAllAttributes (ev) {
-        this.$el.querySelectorAll('td.attributes').forEach(t => this.openAttributes(t))
-        ev.stopPropagation()
-    },
-    closeAllAttributes (ev) {
-        this.$el.querySelectorAll('td.attributes').forEach(t => this.closeAttributes(t))
-        ev.stopPropagation()
-    },
+    //
     attributesClosedText (f) {
         const p = f.gene || f.transcript
         const ptext = p ? ` Parent=${p.label}` : ''
@@ -274,7 +308,7 @@ export default MComponent({
         entries.sort(comparator)
         return entries
     },
-    startcrawl: function (e) {
+    startcrawl (e) {
        const elt = e.target
        const container = elt.closest('.feature-details')
        const rightEdge = container.getBoundingClientRect().right
@@ -289,7 +323,7 @@ export default MComponent({
            }
        }, 25)
     },
-    stopcrawl: function (e) {
+    stopcrawl (e) {
        if (this.interval) window.clearInterval(this.interval)
        this.interval = null
        e.target.style.left = "0px"
@@ -305,14 +339,19 @@ export default MComponent({
 .feature-table td {
     padding-right: 10px;
 }
+/* Disable column collapsing. Kinda clunky right now.
 .feature-table th.collapsed,
 .feature-table td.collapsed {
     max-width: 4px;
     margin-right: 8px;
 }
+*/
 .feature-table-wrapper {
     max-height: 400px;
     overflow: scroll;
+}
+.feature-row.highlighted {
+    background-color: #efefe5 !important;
 }
 .controls {
     position: sticky;
@@ -343,6 +382,7 @@ th > i.material-icons {
 th:hover > i.material-icons {
   opacity: 1;
 }
+/*
 td.attributes.closed tr:nth-child(1) {
   display: inherit;
 }
@@ -355,6 +395,7 @@ td.attributes.closed > table {
 td.attributes.open > span {
     display: none;
 }
+*/
 i.material-icons {
   font-size: 16px;
   cursor: pointer;
@@ -369,15 +410,15 @@ td.attributes i.material-icons {
   left: 0px;
   display: none;
 }
-td.attributes.open:hover i.material-icons.close {
+td.attributes:hover i.material-icons.close {
   display: inherit;
 }
-td.attributes.closed:hover i.material-icons.open {
+td.attributes:hover i.material-icons.open {
   display: inherit;
 }
 /* zebra striping */
 table.feature-table > tr:nth-child(2n) {
-    background-color: #ccc;
+    background-color: #d5d5d5;
 }
 .crawling-text {
     position: relative;
